8640bb2780e30d6042d3b79384d8437e51148803
ajitalapati <ajitalapati01@gmail.com>
2023-01-21 00:21:27 -0600

    lint

--- frontend/src/components/users/CustomAlert.tsx
+++ frontend/src/components/users/CustomAlert.tsx
@@ -4,8 +4,6 @@ import Alert from '@mui/material/Alert';
 import IconButton from '@mui/material/IconButton';
 import Collapse from '@mui/material/Collapse';
 import CloseIcon from '@mui/icons-material/Close';
-import { Dispatch } from 'react';
-import { SetStateAction } from 'react';
 
 interface CustomAlertProps{
   message: string;
