0785fd26fe8554ed74915ebf96c3f36601b93f2c
ajitalapati <ajitalapati01@gmail.com>
2023-01-20 21:18:35 -0600

    fix linter again

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Grid, Typography, Button, Input } from '@mui/material'
 import { constants } from '../constants'
 import { Link } from 'react-router-dom'
