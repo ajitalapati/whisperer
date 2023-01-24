b24da6c2a42e1751ad6cb1bc5d8ac9f7428a1d90
ajitalapati <ajitalapati01@gmail.com>
2023-01-24 16:44:40 -0600

    dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,23 +7,44 @@ import LandingPage from './components/LandingPage/LandingPage'
 import SignIn from './components/users/SignIn';
 import SignUp from './components/users/SignUp';
 import { Account } from './components/users/Account';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    fontFamily: [
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 export const UserContext = createContext({user: ""})
 
 function App() {
   return (
-    <Account>
-        <HashRouter>
-        <NavBar/>
-          <Routes>
-            <Route path="/" element={<LandingPage/>}/>
-            <Route path="/options" element={<OptionPage/>}/>
-            <Route path="/:conversee" element={<Conv/>}/>
-            <Route path="/signin" element={<SignIn/>}/>
-            <Route path="/signup" element={<SignUp/>}/>
-          </Routes>
-        </HashRouter>
-    </Account>
+    <ThemeProvider theme={darkTheme}>
+      <Account>
+          <HashRouter>
+            <NavBar/>
+            <Routes>
+              <Route path="/" element={<LandingPage/>}/>
+              <Route path="/options" element={<OptionPage/>}/>
+              <Route path="/:conversee" element={<Conv/>}/>
+              <Route path="/signin" element={<SignIn/>}/>
+              <Route path="/signup" element={<SignUp/>}/>
+            </Routes>
+          </HashRouter>
+      </Account>
+    </ThemeProvider>
+
   );
 }
 
